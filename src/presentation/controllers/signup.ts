4b54e5dec766cefbd4b7a6dd8356d433e2966af3
Guilherme Jacobsen <guilhermejacobsen@MacBook-Air-de-Guilherme.local>
2023-02-10 20:02:35 -0300

    feat: ensure SignUpController calls AddAccount with correct values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,3 +1,4 @@
+import { AddAccount } from "../../domain/usecases/add-account";
 import { InvalidParamError, MissingParamError } from "../errors";
 import { badRequest, serverError } from "../helpers/http-helpers";
 import {
@@ -9,8 +10,11 @@ import {
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
-  constructor(emailValidator: EmailValidator) {
+  private readonly addAccount: AddAccount;
+
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator;
+    this.addAccount = addAccount;
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
@@ -27,7 +31,7 @@ export class SignUpController implements Controller {
         }
       }
 
-      const { email, password, password_confirmation } = httpRequest.body;
+      const { name, email, password, password_confirmation } = httpRequest.body;
 
       if (password !== password_confirmation) {
         return badRequest(new InvalidParamError("password_confirmation"));
@@ -36,6 +40,12 @@ export class SignUpController implements Controller {
       if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidParamError("email"));
       }
+
+      this.addAccount.add({
+        name,
+        email,
+        password,
+      });
 
       return {
         statusCode: 200,
