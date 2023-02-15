caeeca70fb2cbcb24bb51e47d114bc0311df1dbb
Guilherme Jacobsen <guilhermejacobsen@MacBook-Air-de-Guilherme.local>
2023-02-15 12:20:32 -0300

    test: add mock to EmailValidatorAdapter

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -15,20 +15,21 @@ describe("EmailValidator Adapter", () => {
   test("shoud return false if validator returns false", () => {
     const sut = makeSut();
     jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
-    const isValid = sut.isValid("invalid_email@email.com");
+    const isValid = sut.isValid("invalid_email");
     expect(isValid).toBe(false);
   });
 
   test("shoud return true if validator returns true", () => {
     const sut = makeSut();
-    const isValid = sut.isValid("valid_email@email.com");
+    jest.spyOn(validator, "isEmail").mockReturnValueOnce(true);
+    const isValid = sut.isValid("valid_email");
     expect(isValid).toBe(true);
   });
 
   test("shoud call validator with correct email", () => {
     const sut = makeSut();
     const isEmailSpy = jest.spyOn(validator, "isEmail");
-    sut.isValid("any_email@email.com");
-    expect(isEmailSpy).toHaveBeenCalledWith("any_email@email.com");
+    sut.isValid("any_email");
+    expect(isEmailSpy).toHaveBeenCalledWith("any_email");
   });
 });
