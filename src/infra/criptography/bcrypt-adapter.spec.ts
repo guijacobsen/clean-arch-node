4cc71dfddd48aa128324ef5b16eabeb97f7eacd0
Guilherme Jacobsen <guilhermejacobsen@MacBook-Air-de-Guilherme.local>
2023-02-24 20:29:27 -0300

    feat: ensure BcryptAdapter returns true when compare succeeds

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -46,10 +46,14 @@ describe("Bcrypt Adapter", () => {
 
   test("should call compare method with correct values", async () => {
     const sut = makeSut();
-
     const compareSpy = jest.spyOn(bcrypt, "compare");
-
     await sut.compare("any_value", "any_hash");
     expect(compareSpy).toHaveBeenCalledWith("any_value", "any_hash");
+  });
+
+  test("should retun true when compare method success", async () => {
+    const sut = makeSut();
+    const isValid = await sut.compare("any_value", "any_hash");
+    expect(isValid).toBe(true);
   });
 });
--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -13,7 +13,6 @@ export class BcryptAdapter implements Hasher, HashCompare {
     return hash;
   }
   async compare(value: string, hash: string): Promise<boolean> {
-    await bcrypt.compare(value, hash);
-    return new Promise((resolve) => resolve(true));
+    return await bcrypt.compare(value, hash);
   }
 }
