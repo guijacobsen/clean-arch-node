49aab41cbcd87788910b863521a2ffc672385e56
Guilherme Jacobsen <guilhermejacobsen@MacBook-Air-de-Guilherme.local>
2023-02-24 20:44:31 -0300

    test: ensure BcryptAdapter throw if compare throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -65,4 +65,16 @@ describe("Bcrypt Adapter", () => {
     const isValid = await sut.compare("any_value", "any_hash");
     expect(isValid).toBe(false);
   });
+
+  test("should throw is compare method throws", async () => {
+    const sut = makeSut();
+    jest
+      .spyOn(bcrypt, "compare")
+      .mockImplementationOnce(
+        () => new Promise((resolve, reject) => reject(new Error()))
+      );
+
+    const promise = sut.compare("any_value", "any_hash");
+    await expect(promise).rejects.toThrow();
+  });
 });
