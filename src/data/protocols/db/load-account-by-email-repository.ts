106d19be6f6939afdc64d7b09b7dc79f2718662a
Guilherme Jacobsen <guilhermejacobsen@MacBook-Air-de-Guilherme.local>
2023-02-24 17:07:21 -0300

    refactor: group data layer protocols

--- src/data/protocols/db/load-account-by-email-repository.ts
+++ src/data/protocols/db/load-account-by-email-repository.ts
@@ -1,4 +1,4 @@
-import { AccountModel } from "../usecases/db-add-account/db-account-protocols";
+import { AccountModel } from "../../usecases/db-add-account/db-account-protocols";
 
 export interface LoadAccountByEmailRepository {
   load(email: string): Promise<AccountModel>;
