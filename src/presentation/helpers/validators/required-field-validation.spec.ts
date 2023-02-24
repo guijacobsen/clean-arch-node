cdd8cf063fb83b5978dec97707f6a3818c53eba2
Guilherme Jacobsen <guilhermejacobsen@MacBook-Air-de-Guilherme.local>
2023-02-24 13:28:22 -0300

    test: ensure RequiredFieldValidation not returns if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -8,4 +8,11 @@ describe("RequiredFieldValidation", () => {
     const error = sut.validate({});
     expect(error).toEqual(new MissingParamError("any_field"));
   });
+
+  test("should not return if validation succeeds", () => {
+    const sut = new RequiredFieldValidation("any_field");
+
+    const error = sut.validate({ any_field: "any_field" });
+    expect(error).toBeFalsy();
+  });
 });
